663629444933041347c2847e8fcb933109831d3b
yuuuzzzin <uiuiui118437@gmail.com>
2022-02-07 02:38:31 +0900

    delete files

--- app/src/main/java/com/yuuuzzzin/offoff_android/database/models/Option.kt
+++
@@ -1,5 +0,0 @@
-package com.yuuuzzzin.offoff_android.database.models
-
-data class Option(
-    val title: String,
-)
--- app/src/main/java/com/yuuuzzzin/offoff_android/service/repository/ScheduleServiceRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.yuuuzzzin.offoff_android.service.repository
-
-import com.yuuuzzzin.offoff_android.service.api.BoardService
-import javax.inject.Inject
-
-
-class ScheduleServiceRepository @Inject constructor(private val boardService: BoardService) {
-}
